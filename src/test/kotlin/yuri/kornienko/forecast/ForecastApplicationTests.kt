23403aa47cc9c9677402aecec7abc76533e8d6e8
horoshiy <dynamost@dynamost.ru>
2019-11-17 09:30:04 +0300

    Start init

---
+++ src/test/kotlin/yuri/kornienko/forecast/ForecastApplicationTests.kt
@@ -0,0 +1,13 @@
+package yuri.kornienko.forecast
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForecastApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
